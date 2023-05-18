9aa275eee0e63a5bff685f1d2a5b1d1c393fccc2
thepaperpilot <thepaperpilot@gmail.com>
2023-05-18 15:19:08 +0000

    Deploying to gh-pages from @ thepaperpilot/thepaperpilot.github.io@18e403c8829a50e6bd44225d1c21193c4aac98f2 🚀

--- assets/public_kronos_docs_bars.md.72a3fb7d.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,d as e}from"./app.c95a51e0.js";const u=JSON.parse('{"title":"Bars","description":"","frontmatter":{},"headers":[],"relativePath":"public/kronos/docs/bars.md","lastUpdated":null}'),o={name:"public/kronos/docs/bars.md"},t=e("",6),l=[t];function p(r,i,c,y,d,F){return n(),a("div",null,l)}const b=s(o,[["render",p]]);export{u as __pageData,b as default};
