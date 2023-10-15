ab49ad1a8b0ef6dc8bb444a7776a511f202522c3
thepaperpilot <thepaperpilot@gmail.com>
2023-10-15 06:29:10 +0000

    Deploying to gh-pages from @ thepaperpilot/thepaperpilot.github.io@a88360785e678733d82afa4b902e7c869705ecc1 🚀

--- assets/public_lit_README.md.c7547881.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o,a}from"./app.0f100cc5.js";const m=JSON.parse('{"title":"Kronos","description":"","frontmatter":{},"headers":[],"relativePath":"public/lit/README.md","lastUpdated":null}'),r={name:"public/lit/README.md"},i=a("",4),d=[i];function s(c,l,n,_,p,h){return o(),t("div",null,d)}const f=e(r,[["render",s]]);export{m as __pageData,f as default};
