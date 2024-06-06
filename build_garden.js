c7341a63def4b7281a1df36a86aa2907a4b968d1
thepaperpilot <thepaperpilot@gmail.com>
2024-06-05 23:28:37 -0500

    Fix git log

--- build_garden.js
+++ build_garden.js
@@ -194,7 +194,7 @@ function toSlug(string) {
     // Build changelog
     fs.mkdirSync("./site/changelog");
 
-    const { stdout } = await exec('git log --after="2024-06-03T0:0:0+0000" --pretty=%H site/garden');
+    const { stdout } = await exec('git log master --after="2024-06-03T0:0:0+0000" --pretty=%H site/garden');
     console.log(stdout);
     const entries = await Promise.all(stdout.split("\n").filter(p => p).map(hash => new Promise(async (resolve) => {
         const { stdout: title } = await exec(`git show --quiet --format=%s ${hash}`);
