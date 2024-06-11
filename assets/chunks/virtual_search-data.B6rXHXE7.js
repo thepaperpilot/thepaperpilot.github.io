ef210019c2e7fbea8ee5e42a0a024f4529895cf6
thepaperpilot <thepaperpilot@incremental.social>
2024-06-11 12:56:57 +0000

    Deploying to pages from @ thepaperpilot/pages@ffbee24c2440663df5cca1c8a9d2f9bb5be1a8ca 🚀

---
+++ assets/chunks/virtual_search-data.B6rXHXE7.js
@@ -0,0 +1,1 @@
+const e={map:"[{},{},{},{},{},{},{},{},{}]"},t={},a={previewLength:62,buttonLabel:"Search",placeholder:"Search website",allow:[{},{}],ignore:[]},o={INDEX_DATA:e,PREVIEW_LOOKUP:t,Options:a};export{o as default};
