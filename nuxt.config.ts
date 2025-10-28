206557b4f3c38d3f0472117ee6d3687b295add4f
thepaperpilot <thepaperpilot@gmail.com>
2025-10-28 18:52:35 -0500

    ?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,15 +81,15 @@ export default async () => {
       "/garden/the-small-web": { redirect: "/garden/small-web" },
 
       /** Games from /public need slashes at end to load properly */
-      "^/advent$": { redirect: "/advent/" },
-      "^/dream$": { redirect: "/dream/" },
-      "^/gamedevtree$": { redirect: "/gamedevtree/" },
-      "^/kronos$": { redirect: "/kronos/" },
-      "^/lit$": { redirect: "/lit/" },
-      "^/ludwig$": { redirect: "/ludwig/" },
-      "^/planar$": { redirect: "/planar/" },
-      "^/skilltreetest$": { redirect: "/skilltreetest/" },
-      "^/the_ascension_tree$": { redirect: "/the_ascension_tree/" },
+      "/advent$": { redirect: "/advent/" },
+      "/dream$": { redirect: "/dream/" },
+      "/gamedevtree$": { redirect: "/gamedevtree/" },
+      "/kronos$": { redirect: "/kronos/" },
+      "/lit$": { redirect: "/lit/" },
+      "/ludwig$": { redirect: "/ludwig/" },
+      "/planar$": { redirect: "/planar/" },
+      "/skilltreetest$": { redirect: "/skilltreetest/" },
+      "/the_ascension_tree$": { redirect: "/the_ascension_tree/" },
 
       /** CORS */
       "/_ipx/**": { cors: true },
